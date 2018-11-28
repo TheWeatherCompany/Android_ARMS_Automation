e1b5f3ea256f37d4aea8fe5f27477069cb4cbc8f
naresh.kumar.contractor@weather.com <naresh.kumar.contractor@weather.com>
2018-11-29 00:33:33 +0530

    updated the swipe
    updated the swipe

--- src/test/java/ANDROID_ARMS/ANDROID_ARMS/FeedAdVAlidationonUI.java
+++ src/test/java/ANDROID_ARMS/ANDROID_ARMS/FeedAdVAlidationonUI.java
@@ -155,7 +155,7 @@ extends Drivers
 
 	//Validating Health&Activities Module page Ads
 	//Validating Boat&Beach Module page Ad
-	/*@Test(priority =14,enabled = true)
+	@Test(priority =13,enabled = true)
 		@Title("Validating Extended Boat&Beach page Ad")
 		public void Extended_Boat_beach_Ad() throws Exception
 		{
@@ -167,13 +167,12 @@ extends Drivers
 			AppiumFunctions.Check_boatbeach_bb_ad();
 			//AppFunctions.clickOnBackArrowElement();
 			System.out.println("====== Validating Extended Boat&Beach page Ad TestCase End =======");    
-		}*/
+		}
 		        //Validating Running Module page Ad
 				@Test(priority =12,enabled = true)
 				@Title("Validating Extended Running page Ad")
 				public void Extended_Runningpage_Ad() throws Exception
 				{
-				    AppFunctions.SwipeUp_Counter(2);
 					System.out.println("====== Validating Extended Running page Ad TestCase Started =======");
 					AppFunctions.click_Running_element();
 					AppFunctions.SwipeUp_Counter(3);
@@ -183,7 +182,7 @@ extends Drivers
 					System.out.println("====== Validating Extended Running page Ad TestCase End =======");    
 				}
 				//Validating Cold_flu Module page Ad
-				/*@Test(priority =11,enabled = true)
+				@Test(priority =14,enabled = true)
 				@Title("Validating Extended Cold_flu page Ad")
 				public void Extended_ColdFlu_Ad() throws Exception
 				{
@@ -195,14 +194,14 @@ extends Drivers
 					AppiumFunctions.Check_ColdFlu_bb_ad();
 					//AppFunctions.clickOnBackArrowElement();
 					System.out.println("====== Validating Extended Cold_flu page Ad TestCase End =======");    
-				}*/
+				}
 				//Validating Allergy Module page Ad
-				@Test(priority =13,enabled = true)
+				@Test(priority =11,enabled = true)
 				@Title("Validating Extended Allergy page Ad")
 				public void Extended_Allergy_Ad() throws Exception
 				{
 					System.out.println("====== Validating Extended Allergy page Ad TestCase Started =======");
-					
+					 AppFunctions.SwipeUp_Counter(2);
 					AppFunctions.click_Allergy_element();
 					AppFunctions.SwipeUp_Counter(3);
 					logStep("Checking the Allergy bb ad is presnted or not");
